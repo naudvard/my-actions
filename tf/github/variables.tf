be9b78dcfcab490c663cb3ac48016bf03c421f5e
Nathan AUDVARD <nathan.audvard@adeo.com>
2024-10-22 15:59:09 +0200

    feat: update github projects

--- tf/github/variables.tf
+++ tf/github/variables.tf
@@ -1,12 +1,44 @@
 locals {
   repos = {
-    "test-tf" = {
-      name : "test-tf",
-      description : "Test Terraform",
+    "pignouf-games" = {
+      name : "pignouf-games",
+      description : "Pignouf Games",
+      visibility : "private",
+      branches : ["main"],
+      main : "main",
+      review_count : 1,
+    },
+    "MyLifeManager" = {
+      name : "MyLifeManager",
+      description : "My Life Manager",
+      visibility : "private",
+      branches : ["main"],
+      main : "main",
+      review_count : 0,
+    },
+    "blockchain-vote-svelte" = {
+      name : "blockchain-vote-svelte",
+      description : "Projet scolaire à 2 en nuit blanche pour le 20",
       visibility : "public",
       branches : ["main"],
       main : "main",
-      review_count : 2,
-    }
+      review_count : 1,
+    },
+    "big-cicd-back" = {
+      name : "big-cicd-back",
+      description : "CICD way too complex for a school projet",
+      visibility : "public",
+      branches : ["main"],
+      main : "main",
+      review_count : 1,
+    },
+    "imt-framework-back" = {
+      name : "imt-framework-back",
+      description : "School project, basic delivery app",
+      visibility : "public",
+      branches : ["main"],
+      main : "main",
+      review_count : 1,
+    },
   }
 }
