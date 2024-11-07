ce7596d73247e7382db9676ce6d0c375e887d0bd
naudvard <113362141+naudvard@users.noreply.github.com>
2024-11-07 22:36:41 +0100

    fix(sync): start bash (#4)

--- .github/actions/action-sync-repositories/sync.sh
+++ .github/actions/action-sync-repositories/sync.sh
@@ -3,7 +3,7 @@
 function checkAndPush() {
     cd "$1" || exit
     git add .
-    git diff-index --quiet HEAD || git commit -m "ci: Sync workflows" && git push origin ci/sync
+    git diff-index --quiet HEAD || (git checkout -b ci/sync && git commit -m "ci: Sync workflows" && git push origin ci/sync)
     cd ../
 }
 
@@ -11,19 +11,21 @@ function cloneRepo() {
     git clone "https://$PAT@github.com/naudvard/$1.git"
 }
 
-function sync() {
-    cloneRepo "$source" # clone the source repository
+function copyFile() {
+  mkdir -p "$(dirname "$2")"
+  cp -rf "$1" "$2"
+}
 
+function sync() {
     jq -c '.[]' "$source/$syncJsonPath" | while read -r repo; do
       repo_name=$(echo "$repo" | jq -r '.repository')
       cloneRepo "$repo_name" # clone the destination repository
 
-      echo "$repo" | jq -r '.sync' | while read -r sync; do
+      echo "$repo" | jq -c '.sync[]' | while read -r sync; do
         from=$(echo "$sync" | jq -r '.from')
         to=$(echo "$sync" | jq -r '.to')
 
-        # Copy the files from the source to the destination
-        cp -f "$source/$from" "$repo_name/$to"
+        copyFile "$source/$from" "$repo_name/$to"
       done
 
       # Push the changes to ci/sync branch
@@ -31,12 +33,10 @@ function sync() {
       # Remove the repository folder
       rm -rf "$repo_name"
     done
-
-    # Remove the source repository folder
-    rm -rf "$source"
 }
 
 source="$1"
 syncJsonPath="$2"
 
+cd ../ || exit
 sync
